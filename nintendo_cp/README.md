7188f80187b321e7470043e6314e6d808a97d8db
SHIOYA, Hiromu <kwappa.856@gmail.com>
2013-06-20 20:09:06 +0900

    first commit
     * add question 1

---
+++ nintendo_cp/README.md
@@ -0,0 +1,8 @@
+# Nintendo Code Puzzle
+
+[RubyKaigi2013](http://rubykaigi.org/2013)で配布された任天堂のフライヤーに、RubyのCode Puzzleが掲載されていました。練習問題としてちょうどいいので、Meguro.rbで挑戦してみましょう。
+
+* [Code Puzzle](http://cp1.nintendo.co.jp/)
+ * [Ruby 2013.5 "decode the answer"](http://cp1.nintendo.co.jp/ruby.html)
+ * ![Original Image](http://cp1.nintendo.co.jp/img/ruby_main.jpg)
+
---
+++ nintendo_cp/q02/first_page.rb
@@ -0,0 +1,15 @@
+KEY = ??
+
+def trans_table
+  alphabets = ["A".."Z", "a".."z", "0".."9"].map{|r| r.to_a.join}
+  alphabets.map{|ab| [ab, ab[KEY%ab.size..-1] + ab[0, KEY%ab.size]]}.
+    transpose.map{|a| a.join}
+end
+
+def decode(the_answer)
+  the_answer.tr *trans_table
+end
+
+if __FILE__ == $0
+  puts decode("ykkg://tg2.ezekveuf.tf.ag/vekvi")
+end
