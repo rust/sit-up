16885e22cdb7797394a5ff74b1179d62ad78cb48
SHIOYA, Hiromu <kwappa.856@gmail.com>
2013-06-20 20:20:09 +0900

    [nintendo_cp] add second and third page

---
+++ nintendo_cp/q02/third_page.rb
@@ -0,0 +1,11 @@
+require './utility.rb'
+require './second_page.rb'
+require './number_place.rb'
+
+$grid = NumberPlace::Solver.new($grid).solve()
+
+if __FILE__ == $0
+  puts $grid.to_grid
+  puts
+  puts "step1 answer: #{$grid[0..8]}"
+end
